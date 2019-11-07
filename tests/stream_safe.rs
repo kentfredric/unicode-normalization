553cd4ccc0cf4dd4139f4a038508f49792f96389
Kent Fredric <kentnl@gentoo.org>
2019-11-07 17:11:55 +1300

    Factor stream-safe tests dependent on NORMALIZATION_TESTS to own file

--- tests/stream_safe.rs
+++ tests/stream_safe.rs
@@ -5,18 +5,7 @@ use unicode_normalization::__test_api::{
     decompose_compatible,
     stream_safe,
 };
-mod normalization_tests;
-use normalization_tests::NORMALIZATION_TESTS;
 use std::char;
-
-#[test]
-fn test_normalization_tests_unaffected() {
-    for test in NORMALIZATION_TESTS {
-        for &s in &[test.source, test.nfc, test.nfd, test.nfkc, test.nfkd] {
-            assert_eq!(stream_safe(s), s);
-        }
-    }
-}
 
 #[test]
 fn test_simple() {
