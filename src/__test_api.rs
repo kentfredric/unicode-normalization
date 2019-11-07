b1efa9f06b30931108b5ea45adfa8e518d07a182
Kent Fredric <kentnl@gentoo.org>
2019-11-07 16:41:56 +1300

    Move tests in src/stream_safe.rs to tests/
    Defining various glue intermediaries in __test_api

---
+++ src/__test_api.rs
@@ -0,0 +1,28 @@
+pub use crate::stream_safe::StreamSafe;
+use crate::stream_safe;
+use crate::lookups;
+pub use crate::lookups::canonical_combining_class;
+pub use crate::normalize::decompose_compatible;
+pub use crate::normalization_tests::NORMALIZATION_TESTS;
+
+pub struct Decomposition {
+    pub leading_nonstarters: usize,
+    pub trailing_nonstarters: usize,
+    pub decomposition_len: usize,
+}
+impl Into<Decomposition> for stream_safe::Decomposition {
+    fn into (self: stream_safe::Decomposition) -> Decomposition { 
+            Decomposition {
+                leading_nonstarters: self.leading_nonstarters,
+                trailing_nonstarters: self.trailing_nonstarters,
+                decomposition_len: self.decomposition_len,
+            }
+    }
+}
+
+pub fn classify_nonstarters(c: char) -> Decomposition {
+    stream_safe::classify_nonstarters(c).into()
+}
+pub fn stream_safe(s: &str) -> String {
+        StreamSafe::new(s.chars()).collect()
+}
